581d06f839d583413d6086473383ca7e6566e52d
TobiasKX10 <tobias.kletting@gmail.com>
2025-08-13 18:59:46 +0200

    add project files

---
+++ android/app/src/main/kotlin/com/example/atorch/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atorch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,300 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:torch_light/torch_light.dart';
+import 'dart:isolate';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool active = false;
+  int onTime = 1000;
+  int offTime = 1000;
+  int cycleTime = 1000;
+  int pageIndex = 0;
+  int segmentsCount = 0;
+  bool torch = false;
+  Set<int> selected = Set();
+  Future<void>? runBlink() async {
+    while(active){
+        await TorchLight.enableTorch();
+        await Future.delayed(Duration(milliseconds: onTime));
+        await TorchLight.disableTorch();
+        await Future.delayed(Duration(milliseconds: offTime));
+    }
+    await TorchLight.disableTorch();
+  }
+  Future<void> runRythm() async {
+      while(active){
+        int count = 0;
+        switch(segmentsCount){
+          case 0:
+            count = 4;
+            break;
+          case 1:
+            count = 8;
+            break;
+          case 2:
+            count = 16;
+            break;
+        }
+        for(int i=0; i<count; i++){
+          if(selected.contains(i)&&!torch){
+            await TorchLight.enableTorch();
+            torch = true;
+          }
+          else if(!selected.contains(i)&&torch){
+            await TorchLight.disableTorch();
+            torch = false;
+          }
+          await Future.delayed(Duration(milliseconds: cycleTime));
+        }
+      }
+      await TorchLight.disableTorch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //runBlink();
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Advanced Torch'),
+      ),
+        bottomNavigationBar: NavigationBar(
+          height: 75,
+          backgroundColor: Colors.transparent,
+          labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+          selectedIndex: pageIndex,
+          onDestinationSelected: (int index){
+            active = false;
+            setState(() {
+              pageIndex = index;
+            });
+          },
+          labelTextStyle: WidgetStatePropertyAll<TextStyle>(TextStyle(color: Colors.black,fontSize: 15)),
+          destinations: [
+            NavigationDestination(icon: Icon(Icons.change_circle,color: Color(0xFF1B7D7F)), label: 'Simple'),
+            NavigationDestination(icon: Icon(Icons.segment_outlined,color: Color(0xFF1B7D7F)), label: 'Rythm'),
+          ],
+        ),
+      body:<Widget>[Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 10,
+              children: [
+                Text(active?'On':'Off'),
+                Switch(
+                    value: active,
+                    onChanged: (x) async {
+                      setState(()  {
+                        active = x;
+                      });
+                      if(x){
+                        unawaited(runBlink());
+                      }
+                    }
+                ),
+              ],
+            ),
+            Padding(padding: EdgeInsets.all(24)),
+            Text('Off Duration'),
+            Slider(
+                label: offTime.toString(),
+                value: offTime.toDouble(),
+                year2023: false,
+                max: 10000,
+                min: 0,
+                divisions: 1000,
+                onChanged: (x){
+                  setState(() {
+                    offTime=x.toInt();
+                });
+                }
+            ),
+            Text('${offTime/1000}s'),
+            Padding(padding: EdgeInsets.all(24)),
+            Text('On Duration'),
+            Slider(
+                label: onTime.toString(),
+                value: onTime.toDouble(),
+                year2023: false,
+                max: 10000,
+                min: 0,
+                divisions: 1000,
+                onChanged: (x){
+                  setState(() {
+                    onTime=x.toInt();
+                  });
+                }
+            ),
+            Text('${onTime/1000}s'),
+          ],
+        ),
+      ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16,right: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
+                  children: [
+                    Text(active?'On':'Off'),
+                    Switch(
+                        value: active,
+                        onChanged: (x) async {
+                          setState(()  {
+                            active = x;
+                          });
+                          if(x){
+                            unawaited(runRythm());
+                          }
+                        }
+                    ),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.all(24)),
+                SegmentedButton(
+                  multiSelectionEnabled: true,
+                  emptySelectionAllowed: true,
+                  showSelectedIcon: false,
+                  segments: [[
+                      ButtonSegment<int>(value: 0,label: Text('1')),
+                      ButtonSegment<int>(value: 1,label: Text('2')),
+                      ButtonSegment<int>(value: 2,label: Text('3')),
+                      ButtonSegment<int>(value: 3,label: Text('4')),
+                  ],
+                    [
+                      ButtonSegment<int>(value: 0,label: Text('1')),
+                      ButtonSegment<int>(value: 1,label: Text('2')),
+                      ButtonSegment<int>(value: 2,label: Text('3')),
+                      ButtonSegment<int>(value: 3,label: Text('4')),
+                      ButtonSegment<int>(value: 4,label: Text('5')),
+                      ButtonSegment<int>(value: 5,label: Text('6')),
+                      ButtonSegment<int>(value: 6,label: Text('7')),
+                      ButtonSegment<int>(value: 7,label: Text('8')),
+                    ],
+                    [
+                      ButtonSegment<int>(value: 0,label: Text('1')),
+                      ButtonSegment<int>(value: 1,label: Text('2')),
+                      ButtonSegment<int>(value: 2,label: Text('3')),
+                      ButtonSegment<int>(value: 3,label: Text('4')),
+                      ButtonSegment<int>(value: 4,label: Text('5')),
+                      ButtonSegment<int>(value: 5,label: Text('6')),
+                      ButtonSegment<int>(value: 6,label: Text('7')),
+                      ButtonSegment<int>(value: 7,label: Text('8')),
+                    ]
+                  ][segmentsCount],
+                  selected: selected,
+                  onSelectionChanged:(x){
+                    setState(() {selected=x;});
+                    },
+                ),
+                Visibility(
+                  visible: segmentsCount==2,
+                  child: SegmentedButton(
+                  multiSelectionEnabled: true,
+                  emptySelectionAllowed: true,
+                  showSelectedIcon: false,
+                  segments:
+                  [
+                      ButtonSegment<int>(value: 8,label: Text('9')),
+                      ButtonSegment<int>(value: 9,label: Text('10')),
+                      ButtonSegment<int>(value: 10,label: Text('11')),
+                      ButtonSegment<int>(value: 11,label: Text('12')),
+                      ButtonSegment<int>(value: 12,label: Text('13')),
+                      ButtonSegment<int>(value: 13,label: Text('14')),
+                      ButtonSegment<int>(value: 14,label: Text('15')),
+                      ButtonSegment<int>(value: 15,label: Text('16')),
+                  ],
+                  selected: selected,
+                  onSelectionChanged:(x)=> setState(() {selected=x;}),
+                ),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 12,
+                  children: [
+                    Text('Blocks'),
+                    DropdownButton<int>(
+                      value: segmentsCount,
+                        items: [
+                          DropdownMenuItem(child: Text('4'),value: 0 ,),
+                          DropdownMenuItem(child: Text('8'),value: 1 ,),
+                          DropdownMenuItem(child: Text('16'),value: 2 ,)
+                        ],
+                        onChanged: (x){
+                        setState((){segmentsCount = x!;});
+                        if(x==0){
+                          selected.removeWhere((x)=>x>3);
+                        }
+                        else if(x==1){
+                          selected.removeWhere((x)=>x>7);
+                        }
+                      }
+                    ),
+                  ],
+                ),
+                Padding(padding: EdgeInsets.all(24)),
+                Text('Duration of one Block'),
+                Slider(
+                    label: cycleTime.toString(),
+                    value: cycleTime.toDouble(),
+                    year2023: false,
+                    max: 1000,
+                    min: 0,
+                    divisions: 1000,
+                    onChanged: (x){
+                      setState(() {
+                        cycleTime=x.toInt();
+                      });
+                    }
+                ),
+                Text('${cycleTime/1000}s')
+              ],
+            ),
+          ),
+        )
+      ][pageIndex]
+    );
+  }
+}
